6f576ce5a292193689646a395c65708f19b38cfa
AnujPatel99 <123774547+AnujPatel99@users.noreply.github.com>
2023-02-07 17:51:11 -0500

    Create 904. Fruit Into Baskets.py

---
+++ 904. Fruit Into Baskets.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        d = {}
+        i = 0
+
+        for j, val in enumerate(fruits):
+            d[val] = d.get(val, 0) + 1 # get(key, value to return if key does not exist); increment curr val by 1
+            if len(d) > 2: # if dict has more than 2 types of fruits
+                d[fruits[i]] -= 1 # reduce freq of first picked fruit
+                if d[fruits[i]] == 0: # if first fruit is gone
+                    del d[fruits[i]] # get rid of first fruit
+                i += 1               # increse i to next val
+            print d
+        print j, i
+        return j - i + 1
